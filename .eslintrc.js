0d72c30a5e1ff103ed2f758bc233a3ff9eb383fc
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-03-09 13:17:01 -0400

    lint: Modified eslintrc and initial try linting dir bin start and added new utils file logger.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,7 @@
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    node: true,
-  },
-  parserOptions: {
-    // plugins: ['typescript'],
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    allowImportExportEverywhere: true,
-  },
   extends: [
     'airbnb',
+    'plugin:json/recommended',
     // 'eslint:recommended',
     // 'plugin:react/recommended',
     // 'plugin:@typescript-eslint/recommended',
@@ -24,63 +9,31 @@ module.exports = {
     // 'prettier/react'
     // 'prettier/@typescript-eslint',
   ],
+
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+
   plugins: [
     // '@typescript-eslint',
-    'react',
+    'json',
     'react-hooks'
     // 'prettier',
   ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
+
+  parser: 'babel-eslint',
+  parserOptions: {
+    //ecmaVersion: 2018,
+    //sourceType: 'module',
+    // ecmaFeatures: {
+    //   jsx: true,
+    // },
+    allowImportExportEverywhere: true,
   },
-  rules: {
-    'arrow-parens': 0,
-    'global-require': 0,
-    'no-console': 0,
-    'no-duplicate-imports': 0,
-    'no-underscore-dangle': 0,
-    // 'no-underscore-dangle': [
-    //   2,
-    //   {
-    //     allow: ['__setExecReturnValue', '__setExecuteOnFiles', '__webpack_modules__', '__esModule'],
-    //   },
-    // ],
-    'no-eval': 1,
-    'comma-dangle': [
-      2,
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-        // functions: 'never',
-      },
-    ],
-    //
-    'import/newline-after-import': 0,
-    // 'import/no-default-export': 2,
-    'import/no-duplicates': 2,
-    'import/no-extraneous-dependencies': 0,
-    'import/prefer-default-export': 0,
-    //
-    'react-hooks/exhaustive-deps': 2,
-    'react-hooks/rules-of-hooks': 2,
-    'react/jsx-boolean-value': 1,
-    'react/jsx-closing-bracket-location': 1,
-    'react/jsx-closing-tag-location': 1,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-fragments': 2,
-    'react/jsx-indent': [2, 'tab'],
-    'react/jsx-indent-props': [2, 'tab'],
-    'react/jsx-no-useless-fragment': 2,
-    'react/jsx-pascal-case': 2,
-    'react/jsx-wrap-multilines': 1,
-    'react/no-array-index-key': 1,
-    'react/prop-types': 0,
-  },
+
   globals: {
     __DEVELOPMENT__: true,
     __CLIENT__: true,
@@ -88,5 +41,48 @@ module.exports = {
     __DISABLE_SSR__: true,
     __DEVTOOLS__: true,
     __DLLS__: true,
-  }
+  },
+
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+
+  rules: {
+    'no-lonely-if': 2,
+    'no-nested-ternary': 2,
+    'max-nested-callbacks': [2, { max: 5 }],
+    'constructor-super': 2,
+    'no-this-before-super': 2,
+    'prefer-spread': 2,
+    'arrow-parens': 0,
+    'no-warning-comments': [1, { terms: ['todo', 'fixme'], location: 'start' }],
+    'no-underscore-dangle': 'off',
+    'react/sort-comp': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-fragments': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+    'react/require-extension': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/no-access-state-in-setstate': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'no-param-reassign': 0,
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'import/extensions': [1, { js: 'never' }],
+    'import/max-dependencies': ['error', { max: 40 }],
+    'import/no-extraneous-dependencies': [0],
+  },
+
 }
