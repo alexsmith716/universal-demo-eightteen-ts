67baf511bfd8c087267fee02248177402de0ed3d
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-03-15 21:28:40 -0400

    style: Removed h1 margin top and bottom from container NotFound.

--- src/containers/NotFound/NotFound.tsx
+++ src/containers/NotFound/NotFound.tsx
@@ -6,17 +6,17 @@ export const NotFound = () => {
   const styles = require('./scss/NotFound.scss');
 
   return (
-
     <div className="container">
 
       <Helmet title="Page Not Found" />
 
-      <h1 className={`mt-4 mb-3 ${styles.notFoundUniqueColor}`}>Status Code 404!</h1>
+      <h1 className={styles.notFoundUniqueColor}>Status Code 404!</h1>
 
-      <p>
-        Page Not Found!
-      </p>
-
+      <div className="row">
+        <div className="col">
+          <p>Page Not Found!</p>
+        </div>
+      </div>
     </div>
   );
 }
